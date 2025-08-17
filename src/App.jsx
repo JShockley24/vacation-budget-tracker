5316b4717d963b14bc07c24959754f4626bc6555
Jeff Shockley <jshockley24@gmail.com>
2025-08-16 23:40:18 -0400

    Reorganized files and updated App component

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import VacationBudgetTracker from './VacationBudgetTracker'
+
+export default function App() {
+  return <VacationBudgetTracker />
+}
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import VacationBudgetTracker from './VacationBudgetTracker';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<VacationBudgetTracker />);
